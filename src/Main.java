c9e2b4d05a86b058c0a9cd3917f23ea02f4f698b
JSantoro <jeremie.santoro@heig-vd.ch>
2023-03-09 16:39:11 +0100

    je te donne la main theo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.security.InvalidParameterException;
+
+public class Main extends JFrame {
+    static JFrame f;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 1. Récupération du nombre de chronos à lancer
+        final int chronosMax = 9;
+        final int chronosMin = 1;
+        int nbChronos = Integer.parseInt(args[0]);
+        if(nbChronos > chronosMax || nbChronos < chronosMin) {
+            throw new InvalidParameterException("Le nombre de chronos (passé en paramètre) doit être supérieur à " + chronosMin + " et inférieur à " + chronosMax + "!");
+        }
+
+        // 2. Création de la fenêtre de base
+        f = new JFrame("Panneau de contrôle");
+        f.setSize(900,500);
+
+        // 3. Affichage des options pour CHAQUE chrono
+        /*for(int i = 1; i <= nbChronos; i++) {*/
+            JPanel pnl = new JPanel();
+            JButton start = new JButton("Démarrer");
+            JButton stop = new JButton("Arrêter");
+            JButton reset = new JButton("Réinitialiser");
+            JButton roman = new JButton("Cadran romain");
+            JButton arab = new JButton("Cadran arabe");
+            JButton numeric = new JButton("Numérique");
+            pnl.add(start);
+            pnl.add(stop);
+            pnl.add(reset);
+            pnl.add(roman);
+            pnl.add(arab);
+            pnl.add(numeric);
+            f.add(pnl);
+        //}
+
+
+        // 4. Affichage des options pour intéragir avec TOUS les chronos
+        JPanel allCtrl = new JPanel();
+        JButton allRoman = new JButton("Cadre romain");
+        JButton allArab = new JButton("Cadre arabe");
+        JButton allNumerique = new JButton("Numérique");
+        allCtrl.add(allRoman);
+        allCtrl.add(allArab);
+        allCtrl.add(allNumerique);
+
+        // 5. Ajout du tout à la JFrame principale
+        //f.add(allCtrl);
+        f.setBackground(Color.blue);
+        f.setVisible(true);
     }
 }
