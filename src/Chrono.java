50310c7a5a03db9efc28375d8950070d1b511d2d
JSantoro <jeremie.santoro@heig-vd.ch>
2023-03-14 21:48:55 +0100

    javax les timers sont bons

--- src/Chrono.java
+++ src/Chrono.java
@@ -1,52 +1,35 @@
-import java.util.Timer;
-import java.util.TimerTask;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Chrono extends Subject {
     Chrono() {
         id = ++count;
-        timer = new Timer();
-        time = 0;
-        alreadyRunning = false;
-        stop = false;
-        timerTask = new TimerTask() {
+        ActionListener taskPerformer = new ActionListener() {
             @Override
-            public void run() {
-                try {
-                    while(!stop) {
-                        Thread.sleep(1000);
-                        time++;
-                        System.out.println("Time : " + time);
-                        notifyChrono();
-                    }
-                } catch (InterruptedException e){
-                        throw new RuntimeException(e);
-                    }
+            public void actionPerformed(ActionEvent e) {
+                ++time;
+                notifyChrono();
             }
         };
+        timer = new Timer(delay,taskPerformer);
     }
     static private short count = 0;
+    static private int delay = 1000;
     private final int id;
     private Timer timer;
-    private long time;
-    private Boolean stop;
-    private TimerTask timerTask;
-    private Boolean alreadyRunning;
+    private long time = 0;
     public void demarrer() {
-        if(!alreadyRunning){
-            alreadyRunning = true;
-            timer.schedule(timerTask, 0);
-        }
+        timer.start();
     }
     public void arreter(){
-        stop = true;
-        timer.cancel();
+        timer.stop();
     }
     public void reinitialiser(){
-        arreter();
-        setTime(0);
+        time = 0;
+        notifyChrono();
     }
     public long getTime(){ return time; }
-    public void setTime(long t){ this.time = t; }
     public int getId() { return id;}
     public String getName(){
         return "Chrono #"+getId();
