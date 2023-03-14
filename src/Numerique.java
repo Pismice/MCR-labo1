ca4a479e0e364cf77af6f0ff3851810d4006bd47
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2023-03-14 18:31:03 +0100

    numerique is ok :)

--- src/Numerique.java
+++ src/Numerique.java
@@ -6,10 +6,18 @@ public class Numerique extends AffichageTemporel{
 
     Numerique(Chrono c){
         super(c);
-        JLabel infos = new JLabel(chrono.getName() + ": " + hour + "h " + min + "m " + sec + "s");
+        infos = new JLabel();
+        infos.setText(chrono.getName() + ": " + hour + "h " + min + "m " + sec + "s");
         this.add(infos);
     }
+    JLabel infos;
     public void update(){
         super.update();
+        infos.setText(chrono.getName() + ": " + hour + "h " + min + "m " + sec + "s");
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 }
